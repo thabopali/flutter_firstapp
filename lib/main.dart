c210080ef07c0787470cd5cb6c9207e08b96e818
Pali Thabo <tpali@cut.ac.za>
2020-06-17 14:04:23 +0200

    Styling the app with margins and font sizes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import './question.dart';
+
 //void main() {
 //  runApp(MyApp());
 //}
@@ -40,7 +42,7 @@ class _MyAppState extends State<MyApp> {
           title: Text('My First App'),
         ),
         body: Column(children: [
-          Text(questions[_questionIndex]),
+          Question(questions[_questionIndex]),
           RaisedButton(
             child: Text('Answer 1'),
             onPressed: _answerQuestion,
