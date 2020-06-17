1cfa56f01d2ed7afdd9879c109cf591c7bfb4ce1
Pali Thabo <tpali@cut.ac.za>
2020-06-17 12:42:30 +0200

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+//void main() {
+//  runApp(MyApp());
+//}
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+void answerQuestion(){
+  print('Answer chosen!');
+}
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Column(children: [
+          Text('This question'),
+          RaisedButton(
+            child: Text('Answer 1'),
+            onPressed: answerQuestion,
+          ),
+          RaisedButton(
+            child: Text('Answer 2'),
+            onPressed: null,
+          ),
+          RaisedButton(
+            child: Text('Answer 2'),
+            onPressed: null,
+          ),
+        ]),
+      ),
+    );
+  }
+}
